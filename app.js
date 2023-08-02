d96eb152eeb77f845e0601cb4f208e7ddd4082bb
LRvdLinden <77990847+LRvdLinden@users.noreply.github.com>
2023-08-02 17:15:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,110 +3,110 @@ const Homey = require('homey');
 const Parser = require('rss-parser');
 
 class rssfeedreaderApp extends Homey.App {
-    log() {
-        console.log.bind(this, '[log]').apply(this, arguments);
+  log() {
+    console.log.bind(this, '[log]').apply(this, arguments);
+  }
+
+  error() {
+    console.error.bind(this, '[error]').apply(this, arguments);
+  }
+
+  // -------------------- INIT ----------------------
+
+  onInit() {
+    this.log(`[onInit] ${this.homey.manifest.id} - ${this.homey.manifest.version} gestart...`);
+
+    this.triggerNewArticle = this.homey.flow.getTriggerCard('new_article');
+
+    this.receivedArticleLink = null;
+    this.receivedVideoUrls = new Set(); // Een Set om de ontvangen videolinks bij te houden
+
+    this.checkInterval = 5 * 60 * 1000; // 5 minuten
+    this.parser = new Parser();
+    this.maxFeeds = 10; // Maximale aantal feeds toegestaan
+    this.feedUrls = [];
+    this.parser = new Parser({
+      customFields: {
+        item: [
+          ["media:thumbnail", "thumbnail"]
+        ]
+      }
+    });
+
+    // Haal de feed URLs op uit de instellingen
+    this.loadFeedUrls();
+
+    // Voeg een callback toe voor instellingenupdates
+    this.homey.settings.on('set', this.onSettingsFeedUrls.bind(this));
+
+    // Controleer de RSS-feeds op basis van de opgegeven feed URLs
+    this.checkRssFeeds();
+  }
+
+  async onSettingsFeedUrls({ newSettings, oldSettings, changedKeys }) {
+    // Controleer of de instelling 'feed_urls' is gewijzigd
+    if ('feed_urls' in changedKeys && JSON.stringify(newSettings.feed_urls) !== JSON.stringify(oldSettings.feed_urls)) {
+      this.log('[onSettingsFeedUrls] - Feed URLs zijn bijgewerkt:', newSettings.feed_urls);
+      this.feedUrls = newSettings.feed_urls || [];
+      // Beperk het aantal feeds tot het maximum
+      this.feedUrls = this.feedUrls.slice(0, this.maxFeeds);
+      // Sla de feed URLs op in de instellingen
+      this.homey.settings.set('feed_urls', this.feedUrls);
+      // Haal de nieuwe feeds op
+      this.checkRssFeeds();
     }
+  }
 
-    error() {
-        console.error.bind(this, '[error]').apply(this, arguments);
-    }
+  loadFeedUrls() {
+    // Haal de opgeslagen feed URLs op uit de instellingen
+    this.feedUrls = this.homey.settings.get('feed_urls') || [];
 
-    // -------------------- INIT ----------------------
+    // Beperk het aantal feeds tot het maximum
+    this.feedUrls = this.feedUrls.slice(0, this.maxFeeds);
+  }
 
-    onInit() {
-        this.log(`[onInit] ${this.homey.manifest.id} - ${this.homey.manifest.version} gestart...`);
+  async checkRssFeeds() {
+    try {
+      for (const feedUrl of this.feedUrls) {
+        const feed = await this.parser.parseURL(feedUrl);
 
-        this.triggerNewArticle = this.homey.flow.getTriggerCard('new_article');
+        if (feed && feed.items && feed.items.length) {
+          let [latestItem] = feed.items;
 
-        this.receivedArticleLink = null;
-        this.receivedVideoUrls = new Set(); // Een Set om de ontvangen videolinks bij te houden
+          if (latestItem.title && (latestItem.title.includes('RTL Nieuws') || latestItem.title.includes('RTL Weer'))) {
+            this.log(`[checkRssFeeds] - sla laatste item over vanwege RTL in de titel:`, latestItem.title);
+            [, latestItem] = feed.items;
+          }
 
-        this.checkInterval = 5 * 60 * 1000; // 5 minuten
-        this.parser = new Parser();
-        this.maxFeeds = 10; // Maximale aantal feeds toegestaan
-        this.feedUrls = [];
-        this.parser = new Parser({
-            customFields: {
-                item: [
-                    ["media:thumbnail", "thumbnail"]
-                  ]
-            }
-          });
-        
-        // Haal de feed URLs op uit de instellingen
-        this.loadFeedUrls();
+          this.log(`[checkRssFeeds] - kreeg laatste item:`, latestItem);
+          const { title, link, content, pubDate, thumbnail } = latestItem;
+          const imageUrl = thumbnail && thumbnail.$ && thumbnail.$.url || "";
+          const data = {
+            title,
+            link,
+            content,
+            pubDate,
+            imageUrl
+          };
 
-        // Voeg een callback toe voor instellingenupdates
-        this.homey.settings.on('set', this.onSettingsFeedUrls.bind(this));
+          this.log(`[checkRssFeeds] - trigger nieuw artikel Data:`, data);
 
-        // Controleer de RSS-feeds op basis van de opgegeven feed URLs
-        this.checkRssFeeds();
-    }
+          // Controleer of het nieuwe artikel een andere pubDate heeft dan het laatste getriggerde artikel
+          if (pubDate !== this.lastTriggeredPubDate) {
+            this.log(`[checkRssFeeds] - trigger nieuw artikel Data:`, data);
+            this.triggerNewArticle.trigger(data).catch((err) => this.error('[checkRssFeeds] - Fout bij triggerNewArticle', err));
 
-    async onSettingsFeedUrls({ newSettings, oldSettings, changedKeys }) {
-        // Controleer of de instelling 'feed_urls' is gewijzigd
-        if ('feed_urls' in changedKeys && JSON.stringify(newSettings.feed_urls) !== JSON.stringify(oldSettings.feed_urls)) {
-            this.log('[onSettingsFeedUrls] - Feed URLs zijn bijgewerkt:', newSettings.feed_urls);
-            this.feedUrls = newSettings.feed_urls || [];
-            // Beperk het aantal feeds tot het maximum
-            this.feedUrls = this.feedUrls.slice(0, this.maxFeeds);
-            // Sla de feed URLs op in de instellingen
-            this.homey.settings.set('feed_urls', this.feedUrls);
-            // Haal de nieuwe feeds op
-            this.checkRssFeeds();
+            // Werk de laatste getriggerde pubDate bij naar de huidige pubDate
+            this.lastTriggeredPubDate = pubDate;
+          } else {
+            this.log(`[checkRssFeeds] - Artikel is al getriggerd, overslaan...`);
+          }
         }
+      }
+    } catch (err) {
+      this.error(`[checkRssFeeds] - Fout bij ophalen RSS-feed:`, err);
     }
-
-    loadFeedUrls() {
-        // Haal de opgeslagen feed URLs op uit de instellingen
-        this.feedUrls = this.homey.settings.get('feed_urls') || [];
-
-        // Beperk het aantal feeds tot het maximum
-        this.feedUrls = this.feedUrls.slice(0, this.maxFeeds);
-    }
-
-    async checkRssFeeds() {
-        try {
-            for (const feedUrl of this.feedUrls) {
-                const feed = await this.parser.parseURL(feedUrl);
-
-                if (feed && feed.items && feed.items.length) {
-                    let [latestItem] = feed.items;
-
-                    if (latestItem.title && (latestItem.title.includes('RTL Nieuws') || latestItem.title.includes('RTL Weer'))) {
-                        this.log(`[checkRssFeeds] - sla laatste item over vanwege RTL in de titel:`, latestItem.title);
-                        [, latestItem] = feed.items;
-                    }
-
-                    this.log(`[checkRssFeeds] - kreeg laatste item:`, latestItem);
-                    const { title, link, content, pubDate, thumbnail } = latestItem;
-                    const imageUrl = thumbnail && thumbnail.$ && thumbnail.$.url || "";
-                    const data = {
-                        title,
-                        link,
-                        content,
-                        pubDate,
-                        imageUrl
-                    };
-
-                    this.log(`[checkRssFeeds] - trigger nieuw artikel Data:`, data);
-
-                    // Controleer of het nieuwe artikel een andere pubDate heeft dan het laatste getriggerde artikel
-                    if (pubDate !== this.lastTriggeredPubDate) {
-                        this.log(`[checkRssFeeds] - trigger nieuw artikel Data:`, data);
-                        this.triggerNewArticle.trigger(data).catch((err) => this.error('[checkRssFeeds] - Fout bij triggerNewArticle', err));
-
-                        // Werk de laatste getriggerde pubDate bij naar de huidige pubDate
-                        this.lastTriggeredPubDate = pubDate;
-                    } else {
-                        this.log(`[checkRssFeeds] - Artikel is al getriggerd, overslaan...`);
-                    }
-                }
-            }
-        } catch (err) {
-            this.error(`[checkRssFeeds] - Fout bij ophalen RSS-feed:`, err);
-        }
-    }
+  }
 }
 
 module.exports = rssfeedreaderApp;
